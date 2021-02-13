3b2edc1bf6e8ded874bd15607641feaddd4c4cee
Enis <43475851+Enisizm@users.noreply.github.com>
2021-02-13 18:33:31 +0300

    Update filterwordcheck.js

--- FilterWord/filterwordcheck.js
+++ FilterWord/filterwordcheck.js
@@ -3,7 +3,7 @@ const qdb = require("quick.db");
 const db = new qdb.table("filterayarlar");
 
 module.exports = async (message) => {
-	let filterayar = await db.get("filterayar");
+    let filterayar = await db.get("filterayar");
     let word = filterayar.filteredword;
     if (word.some(word => message.content.includes(word))) {
         message.channel.send(`Mesajında Filtrelenmiş İçerik Bulundu!`)
