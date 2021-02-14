649926b353750b13b84e29ce94280a7cce6a6219
Enis <43475851+Enisizm@users.noreply.github.com>
2021-02-14 20:25:33 +0300

    Update FilteredPanel.js

--- FilterWord/FilteredPanel.js
+++ FilterWord/FilteredPanel.js
@@ -7,30 +7,30 @@ module.exports.execute = async (client, message, args) => {
   if(!conf.sahip.includes(message.author.id))
     if((message.guild.ownerID != message.author.id)) return message.channel.send("**Bunu yapmak için yeterli yetkin yok!**");
   let ozellikler = [
-    { name: "filteredword", type: "word"},
+    { name: "filteredword", type: "cogul"},
   ];
   let secim = args[0];
   const embed = new MessageEmbed().setColor(client.randomColor()).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setFooter("Enis");
   if (secim == "liste") {
-    let data = await db.get("filterayar");
+    let data = await db.get(`filterayar.${message.guild.id}`);
     let ozelliklerListe = Object.keys(data || {}).filter(a => ozellikler.find(v => v.name == a)).map(o => {
       let element = data[o];
       let ozellik = ozellikler.find(z => z.name == o);
-      if(ozellik.type == "word") return `\`${o}\` - ${element.map(tag => `${tag}`).join(', ')}`
+      if(ozellik.type == "cogul") return `\`${o}\` - ${element.map(tag => `${tag}`).join(', ')}`
       
     }).join('\n');
     return message.channel.send(embed.setDescription(ozelliklerListe));
   };
   if (!secim || !ozellikler.some(ozellik => ozellik.name.toLowerCase() == secim.toLowerCase())) return message.channel.send(embed.setDescription(`Bir hata yaptın! Filtrelenmiş Sözcükleri Listeletmek İstiyorsan; **liste** \n\n ${ozellikler.map(o => `\`${o.name}\``).join(", ")} kullanabilirsin.`));
   let ozellik = ozellikler.find(o => o.name.toLowerCase() === secim.toLowerCase());
-    if (ozellik.type == "word"){
+    if (ozellik.type == "cogul"){
     let tag = args.splice(1).join(' ');
     if(!tag) return message.channel.send(embed.setDescription("**Değer belirtmeyi unuttun!**"));
-    let arr = await db.get(`filterayar.${ozellik.name}`) || [];
+    let arr = await db.get(`filterayar.${message.guild.id}.${ozellik.name}`) || [];
     let index = arr.find(e => e == tag);
     if(index) arr.splice(arr.indexOf(tag), 1);
     else arr.push(tag);
-    db.set(`filterayar.${ozellik.name}`, arr);
+    db.set(`filterayar.${message.guild.id}.${ozellik.name}`, arr);
     return message.channel.send(embed.setDescription(`Belirttiğin ayar tanımlandı. Mevcut ayarında \`${arr.join(", ")}\` bulunmakta.`));
   }
 };
