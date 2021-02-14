b1349c1ec06d924f99f9dc7e140f335ae85e84d1
Enis <43475851+Enisizm@users.noreply.github.com>
2021-02-14 20:25:11 +0300

    Update filterwordcheck.js

--- FilterWord/filterwordcheck.js
+++ FilterWord/filterwordcheck.js
@@ -1,13 +1,14 @@
-const Discord = require('discord.js');
+const { Discord , MessageEmbed } = require('discord.js');
 const qdb = require("quick.db");
 const db = new qdb.table("filterayarlar");
 
 module.exports = async (message) => {
-    let filterayar = await db.get("filterayar");
+	let filterayar = await db.get(`filterayar.${message.guild.id}`);
     let word = filterayar.filteredword;
+    let embed = new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setFooter("Cod").setTimestamp();
     if (word.some(word => message.content.includes(word))) {
-        message.channel.send(`Mesajında Filtrelenmiş İçerik Bulundu!`)
-        message.delete();
+        message.channel.send(embed.setDescription("Mesajında  **Filtrelenmiş** Sözcük Bulundu!"));
+        message.delete({timeout:1});
     };
 }
 
