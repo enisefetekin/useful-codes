90e48663be6dfa99e41d504900dab68c841a1e7b
Enis <43475851+Enisizm@users.noreply.github.com>
2021-02-14 20:27:49 +0300

    Update filterwordcheck.js
    The command has been updated to be used separately on all servers.

--- FilterWord/filterwordcheck.js
+++ FilterWord/filterwordcheck.js
@@ -7,7 +7,7 @@ module.exports = async (message) => {
     let word = filterayar.filteredword;
     let embed = new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setFooter("Cod").setTimestamp();
     if (word.some(word => message.content.includes(word))) {
-        message.channel.send(embed.setDescription("Mesajında  **Filtrelenmiş** Sözcük Bulundu!"));
+        message.channel.send(embed.setDescription("Mesajınızda **Filtrelenmiş** Sözcükler Bulundu Lütfen Kullanmayınız!"));
         message.delete({timeout:1});
     };
 }
