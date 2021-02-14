f8efef0dc66a6bbc268acf217c0af167b877c977
Enis <43475851+Enisizm@users.noreply.github.com>
2021-02-14 21:11:17 +0300

    Update FilteredPanel.js

--- FilterWord/FilteredPanel.js
+++ FilterWord/FilteredPanel.js
@@ -7,7 +7,7 @@ module.exports.execute = async (client, message, args) => {
   if(!conf.sahip.includes(message.author.id))
     if((message.guild.ownerID != message.author.id)) return message.channel.send("**Bunu yapmak için yeterli yetkin yok!**");
   let ozellikler = [
-    { name: "filteredword", type: "cogul"},
+    { name: "filteredword", type: "word"},
   ];
   let secim = args[0];
   const embed = new MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setFooter("Enis");
@@ -16,14 +16,14 @@ module.exports.execute = async (client, message, args) => {
     let ozelliklerListe = Object.keys(data || {}).filter(a => ozellikler.find(v => v.name == a)).map(o => {
       let element = data[o];
       let ozellik = ozellikler.find(z => z.name == o);
-      if(ozellik.type == "cogul") return `\`${o}\` - ${element.map(tag => `${tag}`).join(', ')}`
+      if(ozellik.type == "word") return `\`${o}\` - ${element.map(tag => `${tag}`).join(', ')}`
       
     }).join('\n');
     return message.channel.send(embed.setDescription(ozelliklerListe));
   };
   if (!secim || !ozellikler.some(ozellik => ozellik.name.toLowerCase() == secim.toLowerCase())) return message.channel.send(embed.setDescription(`Bir hata yaptın! Filtrelenmiş Sözcükleri Listeletmek İstiyorsan; **liste** \n\n ${ozellikler.map(o => `\`${o.name}\``).join(", ")} kullanabilirsin.`));
   let ozellik = ozellikler.find(o => o.name.toLowerCase() === secim.toLowerCase());
-    if (ozellik.type == "cogul"){
+    if (ozellik.type == "word"){
     let tag = args.splice(1).join(' ');
     if(!tag) return message.channel.send(embed.setDescription("**Değer belirtmeyi unuttun!**"));
     let arr = await db.get(`filterayar.${message.guild.id}.${ozellik.name}`) || [];
